0ca7260600952c4201fb3731aae499e94df4206a
Rui Ito <rui@RuinoMacBook.local>
2019-11-11 22:38:02 +0900

    Style add

--- test.js
+++ test.js
@@ -4,6 +4,7 @@
         let textDiv = document.createElement('div');
         textDiv.id  = "text_div";
         textDiv.innerHTML = "一覧画面です。";
+        textDiv.style = "color: red;";
 
         kintone.app.getHeaderMenuSpaceElement().appendChild(textDiv);
     });
