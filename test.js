ce367fea360f454e313bdccee40a59d705d1fbdf
Rui010 <ito.rui.521@gmail.com>
2019-11-11 22:27:53 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+(function(){
+    "use strict";
+    kintone.events.on('app.record.index.show', function(event) {
+        let textDiv = document.createElement('div');
+        textDiv.id  = "text_div";
+        textDiv.innerHTML = "一覧画面です。";
+
+        kintone.app.getHeaderMenuSpaceElement().appendChild(textDiv;
+    });
+})();
